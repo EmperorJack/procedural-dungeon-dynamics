6e961e1efe5175b102da756dd6cdc82a8ebcdab8
raynerhayl <raynerhaylem@gmail.com>
2017-04-13 12:27:37 +1200

    Initial Commit
    Many things not working in logic script.

---
+++ Crowd Flow/Assets/grid_Structure.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid_Structure : MonoBehaviour {
+
+	public Vector3 start;
+	public Vector3 end;
+
+	// Use this for initialization
+	void Start () {
+
+		Gizmos.DrawLine (start, end);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(Vector3.zero, new Vector3(1, 0, 0), Color.red);	
+	}
+
+	class Cell{
+		double density;
+
+		Cell(double density){
+			this.density = density;
+		}
+
+		Cell(){ this.density = 0.0;}
+
+	}
+}
