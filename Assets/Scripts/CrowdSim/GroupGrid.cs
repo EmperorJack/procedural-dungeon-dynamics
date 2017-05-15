213af64357e7d732dec6acb3800d0d7918a5c3e9
raynerhayl <raynerhaylem@gmail.com>
2017-05-15 21:28:16 +1200

    Potential Gradients calculated
    Potential gradients are calculated and normalized in the groupGrids.

--- Assets/Scripts/CrowdSim/GroupGrid.cs
+++ Assets/Scripts/CrowdSim/GroupGrid.cs
@@ -71,11 +71,91 @@ namespace CrowdSim
 			}
 		}
 
+		//		private void fastMarch ()
+		//		{
+		//			List<GroupCell> known_cells = new List<GroupCell> ();
+		//
+		//			for (int i = 0; i < dim; i++) {
+		//				for (int j = 0; j < dim; j++) {
+		//					GroupCell cell = (GroupCell)grid [i, j];
+		//					if (cell.isGoal) {
+		//						cell.potential = 0f;
+		//						cell.temporary_potential = 0f;
+		//						known_cells.Add (cell);
+		//					} else {
+		//						cell.temporary_potential = float.MaxValue;
+		//						cell.potential = float.MaxValue;
+		//					}
+		//				}
+		//			}
+		//
+		//			SortedList<float, List<GroupCell>> potentials = new SortedList<float, List<GroupCell>> ();
+		//
+		//			foreach (GroupCell knownCell in known_cells) {
+		//				// add neighbours
+		//				foreach (Face face in knownCell.faces) {
+		//					if (face != null && face.neighbour != null) {
+		//						GroupCell candidate = (GroupCell)face.neighbour;
+		//						float estimate = getCellPotential (candidate);
+		//						candidate.temporary_potential = Mathf.Min (candidate.temporary_potential, estimate);
+		//						if (potentials.ContainsKey (candidate.temporary_potential)) {
+		//							potentials [candidate.temporary_potential].Add (candidate);
+		//						} else {
+		//							List<GroupCell> newBucket = new List<GroupCell> ();
+		//							newBucket.Add (candidate);
+		//							potentials.Add (candidate.temporary_potential, newBucket);
+		//						}
+		//					}
+		//				}
+		//				// find min neighbour
+		//			}
+		//
+		//			while (known_cells.Count < dim * dim) {
+		//				// add neighbours of min
+		//				float minKey = potentials.Keys [0];
+		//
+		//				GroupCell minCandidate = (GroupCell)potentials [minKey] [0];
+		//				known_cells.Add (minCandidate);
+		//				// not the right syntax.
+		//				potentials [minKey].Remove (minCandidate);
+		//				if (potentials [minKey].Count == 0) {
+		//					potentials.Remove (minKey);
+		//				}
+		//				minCandidate.potential = minCandidate.temporary_potential;
+		//
+		//				foreach (Face face in minCandidate.faces) {
+		//					if (face != null && face.neighbour != null) {
+		//						GroupCell neighbour = (GroupCell)face.neighbour;
+		//						if (known_cells.Contains (neighbour) == false) {
+		//							float estimate = getCellPotential (neighbour);
+		//							// Remove the old potential value
+		//							if (potentials.ContainsKey (neighbour.temporary_potential)) {
+		//								potentials [neighbour.temporary_potential].Remove (neighbour);
+		//								if (potentials [neighbour.temporary_potential].Count == 0) {
+		//									potentials.Remove (neighbour.temporary_potential);
+		//								}
+		//							}
+		//							// Add the new potential
+		//							neighbour.temporary_potential = Mathf.Min (neighbour.temporary_potential, estimate);
+		//							if (potentials.ContainsKey (neighbour.temporary_potential)) {
+		//								potentials [neighbour.temporary_potential].Add (neighbour);
+		//							} else {
+		//								List<GroupCell> newBucket = new List<GroupCell> ();
+		//								newBucket.Add (neighbour);
+		//								potentials.Add (neighbour.temporary_potential, newBucket);
+		//							}
+		//						}
+		//					}
+		//				}
+		//				// find min
+		//			}
+		//		}
+
 		private void fastMarch ()
 		{
 			List<GroupCell> known_cells = new List<GroupCell> ();
 			List<GroupCell> candidates = new List<GroupCell> ();
-
+		
 			for (int i = 0; i < dim; i++) {
 				for (int j = 0; j < dim; j++) {
 					GroupCell cell = (GroupCell)grid [i, j];
@@ -89,9 +169,9 @@ namespace CrowdSim
 					}
 				}
 			}
-
+		
 			if (known_cells.Count > 0) {
-
+		
 				foreach (GroupCell knownCell in known_cells) {
 					// add neighbours to candidates
 					for (int f_index = 0; f_index < knownCell.faces.Length; f_index++) {
@@ -104,12 +184,12 @@ namespace CrowdSim
 						}
 					}
 				}
-
+		
 				while (known_cells.Count < dim * dim) {
-
+		
 					float min_potential = float.MaxValue;
 					GroupCell min_candidate = null;
-
+		
 					foreach (GroupCell candidate in candidates) {
 						if (candidate.temporary_potential <= min_potential) {
 							//Printer.message ("TEMPPOT(" + candidate.index.x + ", " + candidate.index.y + ") = " + candidate.temporary_potential);
@@ -117,15 +197,16 @@ namespace CrowdSim
 							min_candidate = candidate;
 						}
 					}
-
+		
 					// add candidate with min potential
 					min_candidate.potential = min_candidate.temporary_potential;
 					candidates.Remove (min_candidate);
+
 					//Printer.message ("POT("+min_candidate.index.x+", "+min_candidate.index.y+") = "+min_candidate.potential);
 					max_potential = Mathf.Max (max_potential, min_candidate.potential);
 					known_cells.Add (min_candidate);
-
-
+		
+		
 					// add candidates neighbours
 					foreach (GroupFace face in min_candidate.faces) {
 						if (face != null && face.neighbour != null) {
@@ -143,10 +224,84 @@ namespace CrowdSim
 						}
 					}
 				}
+
+				// iterate through all to compute gradient 
+				for (int i = 0; i < dim; i++) {
+					for (int j = 0; j < dim; j++) {
+						GroupCell cell = (GroupCell)grid [i, j];
+
+						foreach (GroupFace face in cell.faces)
+						{
+							float grad = 0f;
+							if (face != null && face.neighbour != null) {
+								GroupCell neighbour = (GroupCell)face.neighbour;
+								grad = neighbour.potential - cell.potential;
+								face.grad_Potential = grad;
+							}
+
+						}
+						normaliseGrads (cell);
+					}
+				}
+
 			}
 		}
 
-		float getCellPotential (GroupCell cell)
+		void normaliseGrads(GroupCell cell){
+			GroupFace eastFace = (GroupFace)cell.faces [(int)GroupCell.Dir.east];
+			GroupFace westFace = (GroupFace)cell.faces [(int)GroupCell.Dir.west];
+			GroupFace northFace = (GroupFace)cell.faces [(int)GroupCell.Dir.north];
+			GroupFace southFace = (GroupFace)cell.faces [(int)GroupCell.Dir.south];
+
+			float xGrad = 0f;
+			if (eastFace != null && westFace != null) {
+				xGrad = eastFace.grad_Potential - westFace.grad_Potential;
+			}
+
+			float yGrad = 0f;
+			if (northFace != null && southFace != null) {
+				yGrad = northFace.grad_Potential - southFace.grad_Potential;
+			}
+
+			Vector2 newGrads = new Vector2 (xGrad, yGrad);
+			newGrads.Normalize();
+
+			float newXGrad = newGrads.x;
+			float newYGrad = newGrads.y;
+
+			float xRatio = 1f;
+			float yRatio = 1f;
+
+			if (xGrad != 0) {
+				xRatio = newXGrad / xGrad;
+			}
+
+			if (yGrad != 0) {
+				yRatio = newYGrad / yGrad;
+			}
+
+			if (eastFace != null) {
+				eastFace.grad_Potential = eastFace.grad_Potential * xRatio;
+			}
+			if (westFace != null) {
+				westFace.grad_Potential = westFace.grad_Potential * xRatio;
+			}
+			if (northFace != null) {
+				northFace.grad_Potential = northFace.grad_Potential * yRatio;
+			}
+			if (southFace != null) {
+				southFace.grad_Potential = southFace.grad_Potential * yRatio;
+			}
+		}
+
+		void computeVelocity ()
+		{
+
+		}
+
+		// Returns the upwind directions from this cell
+
+		GroupFace[] getUpwind (GroupCell cell)
 		{
 			GroupFace min_x = null;
 			GroupFace min_y = null;
@@ -173,6 +328,15 @@ namespace CrowdSim
 			} else if (south_cost < float.MaxValue) {
 				min_y = (GroupFace)cell.faces [(int)Cell.Dir.south];
 			}
+
+			return new GroupFace[]{ min_x, min_y };
+		}
+
+		float getCellPotential (GroupCell cell)
+		{
+			GroupFace[] upwinds = getUpwind (cell);
+			GroupFace min_x = upwinds [0];
+			GroupFace min_y = upwinds [1];
 
 			if (cell.index.x == 4 && cell.index.y == 4) {
 				if (min_y == null) {
@@ -280,7 +444,7 @@ namespace CrowdSim
 			// TODO: Sometimes a is 0	
 			float a = (cost_one * cost_one) + (cost_two * cost_two);
 			float b = -2 * ((cost_one * cost_one * potential_two) + (cost_two * cost_two * potential_one));
-			float c = (cost_one * cost_one * potential_two * potential_two) + (cost_two * cost_two * potential_one * potential_one)-1;
+			float c = (cost_one * cost_one * potential_two * potential_two) + (cost_two * cost_two * potential_one * potential_one) - 1;
 
 			// TODO: potential_one and Potential_two seem to be zero
 			float under_root = (b * b) - (4 * a * c);
