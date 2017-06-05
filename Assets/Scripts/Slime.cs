7a9e39ba25beb6aa8cc1fa1d825d2fa96c65a18f
EmperorJack <emperorpurvis@gmail.com>
2017-06-05 20:32:34 +1200

    Added slime prefab with animation and script to offset the animation for each instance

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour {
+
+	public void Start()
+	{
+		Animator anim = GetComponent<Animator>();
+		AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+		anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+	}
+}
