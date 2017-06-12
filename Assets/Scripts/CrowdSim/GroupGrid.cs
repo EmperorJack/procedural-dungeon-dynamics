a532cda200d5e75abcb0e3ab81e6f8fda87d537e
raynerhayl <raynerhaylem@gmail.com>
2017-06-13 01:02:41 +1200

    No iteration in potential Calculation
    Removed uneccessary iteration to reset grid cells in the potential calculation. Now it's just done in the main reset.

--- Assets/Scripts/CrowdSim/GroupGrid.cs
+++ Assets/Scripts/CrowdSim/GroupGrid.cs
@@ -54,6 +54,42 @@ namespace CrowdSim
 			}
 		}
 
+		private void removeCell (SortedList<float, List<Cell>> candidates, Cell cell, float oldKey)
+		{
+			if (candidates.ContainsKey (oldKey)) {
+				List<Cell> bucket = candidates [oldKey];
+				bucket.Remove (cell);
+				if (bucket.Count == 0) {
+					candidates.Remove (oldKey);
+				}
+			}
+
+		}
+
+		private void addNeighbours (SortedList<float, List<Cell>> candidates, Cell cell)
+		{
+			foreach (Face face in cell.faces) {
+				Cell neighbour = face.cell;
+				if (neighbour != null && neighbour.isAccepted == false && neighbour.exists) {
+					float tempPotential = calculatePotential (neighbour);
+
+					if (tempPotential < neighbour.potential) {
+
+						removeCell (candidates, neighbour, neighbour.potential);
+
+						if (candidates.ContainsKey (tempPotential)) {
+							candidates [tempPotential].Add (neighbour);
+						} else {
+							List<Cell> bucket = new List<Cell> ();
+							bucket.Add (neighbour);
+							candidates.Add (tempPotential, bucket);
+						}
+						neighbour.potential = tempPotential;
+					}
+				}
+			}
+		}
+
 		private void assignPotentials ()
 		{
 			if (goals.Count == 0) {
@@ -61,39 +97,15 @@ namespace CrowdSim
 			}
 			max = float.MinValue;
 			SortedList<float, List<Cell>> candidates = new SortedList<float, List<Cell>> ();
-			List<Cell> accepted = new List<Cell> ();
-//			for (int i = 0; i < dim; i++) {
-//				for (int j = 0; j < dim; j++) {
-//					if (grid [i, j] != null) {
-//						grid [i, j].reset ();
-//						if (grid [i, j].isGoal) {
-//							grid [i, j].potential = 0;
-//							grid [i, j].isAccepted = true;
-//							accepted.Add (grid [i, j]);
-//							addNeighbours (candidates, grid [i, j]);
-//						} else {
-//							grid [i, j].potential = float.MaxValue;
-//							grid [i, j].isAccepted = false;
-//						}
-//					}
-//				}
-//			}
+			int accepted = 0;
 
 			foreach (Cell goal in goals) {
 				goal.isAccepted = true;
-				accepted.Add (goal);
 				addNeighbours (candidates, goal);
+				accepted++;
 			}
-
-			foreach (Cell cell in accepted) {
-				addNeighbours (candidates, cell);
-			}
-
-//			if (accepted.Count == 0) {
-//				return; // no goals
-//			}
-
-			while (accepted.Count < realCells) { // total number of cells that are connected
+				
+			while (accepted < realCells) { // total number of cells that are connected
 
 				Cell minCandidate = null;
 
@@ -112,8 +124,7 @@ namespace CrowdSim
 				max = Mathf.Max (max, minCandidate.potential);
 
 				addNeighbours (candidates, minCandidate);
-				accepted.Add (minCandidate);
-
+				accepted++;
 			}
 		}
 
@@ -341,44 +352,7 @@ namespace CrowdSim
 				return new Vector2 (0f, 0f);
 			}
 		}
-
-		private void removeCell (SortedList<float, List<Cell>> candidates, Cell cell, float oldKey)
-		{
-			if (candidates.ContainsKey (oldKey)) {
-				List<Cell> bucket = candidates [oldKey];
-				bucket.Remove (cell);
-				if (bucket.Count == 0) {
-					candidates.Remove (oldKey);
-				}
-			}
-
-		}
-
-		private void addNeighbours (SortedList<float, List<Cell>> candidates, Cell cell)
-		{
-			foreach (Face face in cell.faces) {
-				Cell neighbour = face.cell;
-				if (neighbour != null && neighbour.isAccepted == false && neighbour.exists) {
-					float tempPotential = calculatePotential (neighbour);
-
-					if (tempPotential < neighbour.potential) {
-
-						removeCell (candidates, neighbour, neighbour.potential);
-
-						if (candidates.ContainsKey (tempPotential)) {
-							candidates [tempPotential].Add (neighbour);
-						} else {
-							List<Cell> bucket = new List<Cell> ();
-							bucket.Add (neighbour);
-							candidates.Add (tempPotential, bucket);
-						}
-						neighbour.potential = tempPotential;
-					}
-				}
-			}
-		}
-
-
+			
 		private float calculatePotential (Cell cell)
 		{
 			Cell sharedCell = cell.sharedCell;
